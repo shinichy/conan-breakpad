47680cc720f3e88593c3db56805048d5b924d23d
Shinichi Katayama <shin1.katayama@gmail.com>
2017-10-10 14:00:43 +0900

    Fix CI (#4)
    * Fix AppVeyor failure
    
    * Update build.py
    
    * Change CONAN_UPLOAD
    
    * Remove CONAN_UPLOAD

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-  builder = ConanMultiPackager( username="shinichy", channel="testing" )
+  builder = ConanMultiPackager()
   builder.add_common_builds( pure_c=False )
   filtered_builds = []
-  for settings, options in builder.builds:
+  for settings, options, env_vars, build_requires in builder.builds:
     if settings["compiler"] == "apple-clang" or (settings["compiler.version"] == "14" and settings["compiler.runtime"] in ["MD", "MDd"]):
-      filtered_builds.append([settings, options])
+      filtered_builds.append([settings, options, env_vars, build_requires])
   builder.builds = filtered_builds
   builder.run()
